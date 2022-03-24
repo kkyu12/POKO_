401b819ea67c23df1e777c449c0e4e71f6ee18da
kkyu12 <101681296+kkyu12@users.noreply.github.com>
2022-03-24 10:53:41 +0900

    권종별 날짜별 우대권 매출 현황

--- 롯데월드 매출내역서1.cpp
+++ 롯데월드 매출내역서1.cpp
@@ -7,9 +7,9 @@ int main()
 	int year[MAX],month[MAX],day[MAX],ticketType[MAX],ticketTime[MAX],age[MAX],
 		count[MAX],ticketPrice[MAX],typeCom[MAX];
 	int index=0, k=0;
-	int type[4][7]={0};
-	int time[MAX][4]={0};
-	int com[7]={0};
+	int type[4][7]={0};//권종별 
+	int time[MAX][4]={0};//날짜별 
+	int com[7]={0};//우대별 
 	
 	while(fscanf(fp,"%d,%d,%d,%d,%d,%d,%d,%d,%d",&year[index],&month[index],&day[index],
 					&ticketType[index],&ticketTime[index],&age[index],&count[index],
@@ -17,7 +17,8 @@ int main()
 		index++;
 	}
 	fclose(fp);
-	//매출 날짜, 종류, 총 출력 
+	
+//매출 날짜, 종류, 총 출력 
 	for (int i=0;i<index;i++){
 		printf("%d,%d,%d,%d,%d,%d,%d,%d,%d\n",year[i],month[i],day[i],
 					ticketType[i],ticketTime[i],age[i],count[i],
@@ -25,7 +26,7 @@ int main()
 	}
 	printf("\n\n");
 	
-	//권종별 매출현황
+//권종별 매출현황
 	for (int i=0;i<index;i++){
 		if (ticketType[i]==1 && ticketTime[i]==1){
 			type[0][0]+=count[i];
@@ -47,7 +48,6 @@ int main()
 					type[0][5]+=count[i];
 					break;
 			}
-			
 		}
 		else if (ticketType[i]==1 && ticketTime[i]==2){
 			type[1][0]+=count[i];
@@ -112,39 +112,39 @@ int main()
 					break;
 			}
 		}
-		
 	}
+	
 	printf("================== 권종 별 판매현황 ==================\n\n");
-	printf("종합이용권+ 1Day 총 %d매\n", type[0][0]);
+	printf("종합이용권 + 1Day 총 %d매\n", type[0][0]);
 	printf("노인 %d매, 성인 %d매,청소년 %d매, 어린이 %d매, 유아 %d매\n",
 						type[0][1],type[0][2],type[0][3],type[0][4],type[0][5]);
-	printf("종합이용권+ 1Day 총 매출 : %d원\n\n", type[0][6]);
+	printf("종합이용권 + 1Day 총 매출 : %d원\n\n", type[0][6]);
 	
-	printf("종합이용권+ After4 총 %d매\n", type[1][0]);
+	printf("종합이용권 + After4 총 %d매\n", type[1][0]);
 	printf("노인 %d매, 성인 %d매,청소년 %d매, 어린이 %d매, 유아 %d매\n",
 						type[1][1],type[1][2],type[1][3],type[1][4],type[1][5]);
-	printf("종합이용권+ After4 총 매출 : %d원\n\n", type[1][6]);
+	printf("종합이용권 + After4 총 매출 : %d원\n\n", type[1][6]);
 	
-	printf("파크이용권+ 1Day 총 %d매\n", type[2][0]);
+	printf("파크이용권 + 1Day 총 %d매\n", type[2][0]);
 	printf("노인 %d매, 성인 %d매,청소년 %d매, 어린이 %d매, 유아 %d매\n",
 						type[2][1],type[2][2],type[2][3],type[2][4],type[2][5]);
-	printf("파크이용권+ 1Day 총 매출 : %d원\n\n", type[2][6]);
+	printf("파크이용권 + 1Day 총 매출 : %d원\n\n", type[2][6]);
 	
-	printf("파크이용권+ After4 총 %d매\n", type[3][0]);
+	printf("파크이용권 + After4 총 %d매\n", type[3][0]);
 	printf("노인 %d매, 성인 %d매,청소년 %d매, 어린이 %d매, 유아 %d매\n",
 						type[3][1],type[3][2],type[3][3],type[3][4],type[3][5]);
-	printf("파크이용권+ After4 총 매출 : %d원\n\n", type[3][6]);
+	printf("파크이용권 + After4 총 매출 : %d원\n\n", type[3][6]);
 	printf("======================================================\n\n");
 	
 	FILE *fp1 = fopen("report1.csv", "w");
+	fprintf(fp1,"총 매수,노인,성인,청소년,어린이,유아,총 매출\n");
 	for (int i=0;i<4;i++){
 		fprintf(fp1,"%d,%d,%d,%d,%d,%d,%d\n",type[i][0],type[i][1],type[i][2]
-									,type[i][3],type[i][4],type[i][5],type[i][6]);
+								,type[i][3],type[i][4],type[i][5],type[i][6]);
 	}
 	fclose(fp1);
 	
-	//날짜별 매출현황 
-	
+//날짜별 매출현황 
 	time[k][0]=year[0],time[k][1]=month[0],time[k][2]=day[0],time[k][3]=ticketPrice[0];
 	for (int i=1;i<index;i++){
 		if (time[k][0]==year[i] && time[k][1]==month[i] && time[k][2]==day[i]){
@@ -155,17 +155,19 @@ int main()
 			time[k][0]=year[i],time[k][1]=month[i],time[k][2]=day[i],time[k][3]=ticketPrice[i];
 		}
 	}
-	
+	printf("==== 날짜별 매출 현황 ====\n");
 	for (int i=0;i<=k;i++)
-		printf("%d %d %d %d\n",time[i][0],time[i][1],time[i][2],time[i][3]);
+		printf("%d년 %02d월 %02d일 %d원\n",time[i][0],time[i][1],time[i][2],time[i][3]);
 	printf("\n");
 	
 	FILE *fp2 = fopen("report2.csv", "w");
-	for (int i=0;i<=k;i++)
-	fprintf(fp,"%d,%d,%d,%d\n",time[i][0],time[i][1],time[i][2],time[i][3]);
+	fprintf(fp2,"년,월,일,총 매출\n");
+	for (int i=0;i<=k;i++){
+		fprintf(fp,"%d,%d,%d,%d\n",time[i][0],time[i][1],time[i][2],time[i][3]);
+	}
 	fclose(fp2);
 	
-	//우대권 매출현황 
+//우대권 매출현황 
 	for (int i=0;i<index;i++){
 		com[0]+=count[i];
 		switch (typeCom[i]){
@@ -201,7 +203,14 @@ int main()
 	printf("============================\n");
 	
 	FILE *fp3 = fopen("report3.csv", "w");
-	fprintf(fp3,"%d,%d,%d,%d,%d,%d,%d\n",com[0],com[1],com[2],com[3],com[4],com[5],com[6]);
+	fprintf(fp3,"구분,우대권\n");
+	fprintf(fp3,"우대없음,%d\n",com[1]);
+	fprintf(fp3,"장애인,%d\n",com[2]);
+	fprintf(fp3,"국가유공자,%d\n",com[3]);
+	fprintf(fp3,"휴가장병,%d\n",com[4]);
+	fprintf(fp3,"임산부,%d\n",com[5]);
+	fprintf(fp3,"다둥이 행복카드,%d\n",com[6]);
+	fprintf(fp3,"합계,%d\n",com[0]);
 	fclose(fp3);
 	
 	return 0;
